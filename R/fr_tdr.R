e2ba744e6d4833a1c0111014adedde41dea9c1d5
Cole Brokamp <cole.brokamp@gmail.com>
2023-08-26 20:09:57 -0400

    try some fr_tdr stuff..?

--- R/fr_tdr.R
+++ R/fr_tdr.R
@@ -1,6 +1,39 @@
-new_fr_tdr <- function(...) {
-  x <- rlang::list2(...)
-
+new_fr_tdr <- function(x,
+                       name = character(),
+                       version = character(),
+                       title = character(),
+                       description = character(),
+                       homepage = character()) {
+  ## if(!all(sapply(x, is_fr_field))) {
+  ##   rlang::abort("not all variables in `x` are `tdr_field` objects")
+  ## }
+  rlang::check_required(x)
+  x <- as.list(x)
+  if (!rlang::is_character(name)) {
+    rlang::abort("`name` must be a character vector.")
+  }
+  vctrs::vec_check_size(name, size = 1L)
+  if (!rlang::is_character(title)) {
+    rlang::abort("`name` must be a character vector.")
+  }
+  if (!rlang::is_character(description)) {
+    rlang::abort("`description` must be a character vector.")
+  }
+  if (!rlang::is_character(version)) {
+    rlang::abort("`version` must be a character vector.")
+  }
+  if (!rlang::is_character(homepage)) {
+    rlang::abort("`homepage` must be a character vector.")
+  }
+  vctrs::new_list_of(
+    x,
+    ptype = fr_field(character(), name = "hey"),
+    name = name,
+    version = version[1],
+    title = title[1],
+    description = description[1],
+    homepage = homepage[1],
+    class = "fr_tdr")
 }
 
 
--- tests/testthat/test-fr_tdr.R
+++ tests/testthat/test-fr_tdr.R
@@ -1,6 +1,17 @@
-## x <-
-##   tibble::tibble(
-##     id = fr_field(letters)
+test_that("fr_tdr works", {
+  x <-
+    tibble::tibble(
+      id = fr_field(letters, name = "id"),
+      score = fr_field(rnorm(26), name = "score"),
+      case = fr_field(sample(c(TRUE ,FALSE), 26, replace = TRUE),
+                      name = "case",
+                      description = "true if this person was a case")
+    )
+  new_fr_tdr(x, name = "my_dataset") |>
+    attributes()
+    expect_s3_class("list")
+})
+
 
 
 ## the_md <- yaml::read_yaml("https://github.com/geomarker-io/hamilton_property_code_enforcement/releases/download/0.1.2/tabular-data-resource.yaml")
