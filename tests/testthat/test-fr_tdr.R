15b5701586a6ccb02315dccdc1ecb1ea9f6e5f23
Cole Brokamp <cole.brokamp@gmail.com>
2023-09-17 22:04:43 -0500

    cleanup

--- tests/testthat/test-fr_tdr.R
+++ tests/testthat/test-fr_tdr.R
@@ -20,14 +20,4 @@ test_that("fr_tdr works", {
 
   expect_identical(as_tbl_df(as_fr_tdr(mtcars, "mtcars")),
                    tibble::as_tibble(mtcars))
-
 })
-
-
-
-## the_md <- yaml::read_yaml("https://github.com/geomarker-io/hamilton_property_code_enforcement/releases/download/0.1.2/tabular-data-resource.yaml")
-
-
-## field_md_2 <- the_md$schema$fields[[2]]
-
-## fr_field(x = 1:10, !!!field_md_2, type = "number")
