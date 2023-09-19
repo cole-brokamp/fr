29a8913138511cfa63a393321171670e2be70e53
Cole Brokamp <cole.brokamp@gmail.com>
2023-09-19 12:17:48 -0500

    one more test for removing constraint on print

--- tests/testthat/test-fr_desc.R
+++ tests/testthat/test-fr_desc.R
@@ -18,4 +18,21 @@ test_that("fr_desc works for fr_field", {
       )
     )
 
+  no_foofy <-
+    as_fr_field(letters[1:3],
+      name = "foofy",
+      title = "Foofy",
+      description = "This is just a placeholder for a description."
+    )
+
+  fr_desc(no_foofy) |>
+    expect_identical(
+      list(
+        name = "foofy",
+        type = "string",
+        title = "Foofy",
+        description = "This is just a placeholder for a description."
+      )
+    )
+
 })
