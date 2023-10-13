1cbf2c8e2a1e6d41e40665e4252d9d20261f7efe
Cole Brokamp <cole.brokamp@gmail.com>
2023-10-13 09:27:32 -0400

    skip read_fr_tdr url tests when offline

--- tests/testthat/test-read_fr_tdr.R
+++ tests/testthat/test-read_fr_tdr.R
@@ -1,5 +1,4 @@
 test_that("read_fr_tdr works", {
-
   read_fr_tdr(x = test_path("hamilton_poverty_2020")) |>
     expect_s3_class("fr_tdr")
 
@@ -7,11 +6,13 @@ test_that("read_fr_tdr works", {
   expect_identical(levels(d_tdr$rating), c("good", "better", "best"))
   expect_identical(attr(d_tdr, "name"), "example_data_with_types")
   expect_identical(attr(d_tdr, "path"), "example_data_with_types.csv")
+})
 
-  read_fr_tdr("https://github.com/geomarker-io/curated_violations/releases/download/0.1.2/tabular-data-resource.yaml") |>
-    expect_s3_class("fr_tdr")
+test_that("read_fr_tdr works with url", {
+  skip_if_offline()
+
+  expect_s3_class("fr_tdr")
 
   read_fr_tdr("https://github.com/geomarker-io/curated_violations/releases/download/0.1.2") |>
     expect_s3_class("fr_tdr")
-
 })
