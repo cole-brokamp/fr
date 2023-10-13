719535b4ab87f4dfe9749dcd9a702e061ad65e97
Cole Brokamp <cole.brokamp@gmail.com>
2023-10-12 22:21:16 -0400

    read_fr_tdr can read from a url

--- R/read_fr_tdr.R
+++ R/read_fr_tdr.R
@@ -1,3 +1,6 @@
+is_url <- function(x) grepl("^((http|ftp)s?|sftp)://", x)
+has_tdr_yaml <- function(x) grepl("tabular-data-resource.yaml", x)
+
 #' read a tabular-data-resource into R
 #'
 #' @param x path to a `tabular-data-resource.yaml` file
@@ -5,12 +8,37 @@
 #' @return a [fr_tdr][fr::fr-package] object
 #' @export
 read_fr_tdr <- function(x, ...) {
-  fr_descs <- yaml::read_yaml(x)
-  profile <- purrr::pluck(fr_descs, "profile", .default = NA)
-  if (is.na(profile) || {profile != "tabular-data-resource"}) {
-    stop("profile must be 'tabular-data-resource' but is ", profile, call. = FALSE)
+  if (has_tdr_yaml(x)) {
+    tdr_path <- x
+  } else {
+    if (is_url(x)) {
+      tdr_path <- paste0(x, "/tabular-data-resource.yaml")
+    } else {
+      tdr_path <- fs::path(x, "tabular-data-resource.yaml")
     }
-  fr_csv_path <- fs::path(fs::path_dir(x), fr_descs$path)
+  }
+
+  fr_descs <- yaml::read_yaml(tdr_path)
+  profile <- purrr::pluck(fr_descs, "profile", .default = NA)
+  if (is.na(profile) || {
+    profile != "tabular-data-resource"
+  }) {
+    stop("profile must be 'tabular-data-resource' but is ", profile, call. = FALSE)
+  }
+
+  if (has_tdr_yaml(x)) {
+    if (is_url(x)) {
+      fr_csv_path <- sub("tabular-data-resource.yaml", fr_descs$path, x, fixed = TRUE)
+    } else {
+      fr_csv_path <- fs::path(fs::path_dir(x), fr_descs$path)
+    }
+  } else {
+    if (is_url(x)) {
+      fr_csv_path <- paste0(x, "/", fr_descs$path)
+    } else {
+      fr_csv_path <- fs::path(x, fr_descs$path)
+    }
+  }
 
   flds <- purrr::pluck(fr_descs, "schema", "fields")
 
@@ -54,14 +82,16 @@ read_fr_tdr <- function(x, ...) {
   pluck_char <- function(x, y) purrr::pluck(x, y, .default = character())
 
   out <-
-    fr_tdr(name = pluck_char(fr_descs, "name"),
-           path = pluck_char(fr_descs, "path"),
-           version = pluck_char(fr_descs, "version"),
-           title = pluck_char(fr_descs, "title"),
-           homepage = pluck_char(fr_descs, "homepage"),
-           description = pluck_char(fr_descs, "description"),
-           schema = fr_schema(fields = lapply(fr_descs$schema$fields, as_fr_field)),
-           data = the_data)
+    fr_tdr(
+      name = pluck_char(fr_descs, "name"),
+      path = pluck_char(fr_descs, "path"),
+      version = pluck_char(fr_descs, "version"),
+      title = pluck_char(fr_descs, "title"),
+      homepage = pluck_char(fr_descs, "homepage"),
+      description = pluck_char(fr_descs, "description"),
+      schema = fr_schema(fields = lapply(fr_descs$schema$fields, as_fr_field)),
+      data = the_data
+    )
 
   return(out)
 }
--- tests/testthat/test-read_fr_tdr.R
+++ tests/testthat/test-read_fr_tdr.R
@@ -1,11 +1,17 @@
 test_that("read_fr_tdr works", {
 
-  read_fr_tdr(x = test_path("hamilton_poverty_2020", "tabular-data-resource.yaml")) |>
-    expect_snapshot()
+  read_fr_tdr(x = test_path("hamilton_poverty_2020")) |>
+    expect_s3_class("fr_tdr")
 
   d_tdr <- read_fr_tdr(x = test_path("example_data_with_types", "tabular-data-resource.yaml"))
   expect_identical(levels(d_tdr$rating), c("good", "better", "best"))
   expect_identical(attr(d_tdr, "name"), "example_data_with_types")
   expect_identical(attr(d_tdr, "path"), "example_data_with_types.csv")
 
-  })
+  read_fr_tdr("https://github.com/geomarker-io/curated_violations/releases/download/0.1.2/tabular-data-resource.yaml") |>
+    expect_s3_class("fr_tdr")
+
+  read_fr_tdr("https://github.com/geomarker-io/curated_violations/releases/download/0.1.2") |>
+    expect_s3_class("fr_tdr")
+
+})
