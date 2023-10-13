fa306301e0ebcd39bc4306624d764ffad251e301
Cole Brokamp <cole.brokamp@gmail.com>
2023-10-13 13:26:33 -0400

    test to use local tdr on gh

--- tests/testthat/test-read_fr_tdr.R
+++ tests/testthat/test-read_fr_tdr.R
@@ -1,8 +1,8 @@
 test_that("read_fr_tdr works", {
-  read_fr_tdr(x = test_path("hamilton_poverty_2020")) |>
+  read_fr_tdr(test_path("hamilton_poverty_2020")) |>
     expect_s3_class("fr_tdr")
 
-  d_tdr <- read_fr_tdr(x = test_path("example_data_with_types", "tabular-data-resource.yaml"))
+  d_tdr <- read_fr_tdr(file = test_path("example_data_with_types", "tabular-data-resource.yaml"))
   expect_identical(levels(d_tdr$rating), c("good", "better", "best"))
   expect_identical(attr(d_tdr, "name"), "example_data_with_types")
   expect_identical(attr(d_tdr, "path"), "example_data_with_types.csv")
@@ -10,9 +10,11 @@ test_that("read_fr_tdr works", {
 
 test_that("read_fr_tdr works with url", {
   skip_if_offline()
-  read_fr_tdr("https://github.com/geomarker-io/curated_violations/releases/download/0.1.2/tabular-data-resource.yaml") |>
+
+  read_fr_tdr("https://raw.githubusercontent.com/cole-brokamp/fr/main/inst/hamilton_poverty_2020/tabular-data-resource.yaml") |>
     expect_s3_class("fr_tdr")
 
-  read_fr_tdr("https://github.com/geomarker-io/curated_violations/releases/download/0.1.2") |>
+  read_fr_tdr("https://raw.githubusercontent.com/cole-brokamp/fr/main/inst/hamilton_poverty_2020") |>
     expect_s3_class("fr_tdr")
+
 })
