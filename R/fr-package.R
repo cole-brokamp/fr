5b0846051767a08ebcf512e1903647b60a84a872
Cole Brokamp <cole.brokamp@gmail.com>
2023-10-31 15:44:50 -0400

    import @ from S7 if R version older than 4.3.0

--- R/fr-package.R
+++ R/fr-package.R
@@ -10,5 +10,9 @@ NULL
   S7::methods_register()
 }
 
+# enable usage of <S7_object>@name in package code
+#' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
+NULL
+
 utils::globalVariables(c("path", "enum", "schema", "fields"))
 
